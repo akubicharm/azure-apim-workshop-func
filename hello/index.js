dbf7cbca6256d4c8127a985bbdc171978ce30eda
大溝桂 <kay.akubicharm.dev@gmail.com>
2023-05-26 07:04:09 +0900

    update

--- hello/index.js
+++ hello/index.js
@@ -3,11 +3,16 @@ module.exports = async function (context, req) {
 
     const name = (req.query.name || (req.body && req.body.name));
 
+    auth = context.req.headers.authorization;
     context.log(context.req.headers.authorization)
 
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+    // const responseMessage = name
+    //     ? "Hello, " + name + ". This HTTP triggered function executed successfully."
+    //     : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+
+    const responseMessage = auth
+        ? "Authorization: "  + auth
+        : "No Authorization header found";
 
     context.res = {
         // status: 200, /* Defaults to 200 */
